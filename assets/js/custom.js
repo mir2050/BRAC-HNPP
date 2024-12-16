b14837ccac9d48c6b8e24c0058f85a6e7258f034
mir2050 <mirbellalhossen2020@gmail.com>
2024-12-16 22:00:55 +0600

    first commit

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,14 +1,20 @@
+$(document).ready(function() {
 
-$(window).scroll(function () {
-  if (!(jQuery('body > main').hasClass('non-sticky'))) {
-      if (jQuery(window).scrollTop()) {
-          jQuery(".top-menu").addClass("slideDownScaleReversedIn").removeClass("slideDownScaleReversedOut");
-      }
-      else {
-          jQuery(".top-menu").addClass("slideDownScaleReversedOut").removeClass("slideDownScaleReversedIn");
-      }
-  }
-  else {
-      console.log('sticky');
-  }
-});
+    $(window).scroll(function () {
+    if (!(jQuery('body > main').hasClass('non-sticky'))) {
+        if (jQuery(window).scrollTop()) {
+            jQuery(".top-menu").addClass("slideDownScaleReversedIn").removeClass("slideDownScaleReversedOut");
+        }
+        else {
+            jQuery(".top-menu").addClass("slideDownScaleReversedOut").removeClass("slideDownScaleReversedIn");
+        }
+    }
+    else {
+        console.log('sticky');
+    }
+    });
+
+    // For Gallery
+    $('.mix-active').mixItUp();
+
+});    
